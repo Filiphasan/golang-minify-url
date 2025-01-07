663e1b1ae903d6f862fc0e88eeedb7bb043ba0f9
Hasan Erdal <hasaerda@hotmail.com>
2025-01-07 21:29:52 +0300

    redis context ayarı yapıldı ve redis servisi dolduruldu

--- internal/app/caches/cache.go
+++ internal/app/caches/cache.go
@@ -1,13 +1,16 @@
 package caches
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Cache interface {
-	Ping() error
-	Get(key string) (string, error)
-	Set(key string, value interface{}, ttl time.Duration) error
-	Exist(key string) bool
-	Remove(key string) error
-	AddList(key string, isRight bool, value interface{}) error
-	ListPop(key string, isRight bool) (string, error)
+	Ping(ctx context.Context) error
+	Get(ctx context.Context, key string) (string, error)
+	Set(ctx context.Context, key string, value interface{}, ttl time.Duration) error
+	Exist(ctx context.Context, key string) bool
+	Remove(ctx context.Context, key string) error
+	AddList(ctx context.Context, key string, isRight bool, value interface{}) error
+	ListPop(ctx context.Context, key string, isRight bool) (string, error)
 }
--- internal/app/caches/redis.go
+++ internal/app/caches/redis.go
@@ -1,6 +1,7 @@
 package caches
 
 import (
+	"context"
 	"github.com/redis/go-redis/v9"
 	"time"
 )
@@ -15,37 +16,39 @@ func NewRedisCache(client *redis.Client) *RedisCache {
 	}
 }
 
-func (r RedisCache) Ping() error {
-	//TODO implement me
-	panic("implement me")
+func (r RedisCache) Ping(ctx context.Context) error {
+	return r.client.Ping(ctx).Err()
 }
 
-func (r RedisCache) Get(key string) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (r RedisCache) Get(ctx context.Context, key string) (string, error) {
+	return r.client.Get(ctx, key).Result()
 }
 
-func (r RedisCache) Set(key string, value interface{}, ttl time.Duration) error {
-	//TODO implement me
-	panic("implement me")
+func (r RedisCache) Set(ctx context.Context, key string, value interface{}, ttl time.Duration) error {
+	return r.client.Set(ctx, key, value, ttl).Err()
 }
 
-func (r RedisCache) Exist(key string) bool {
-	//TODO implement me
-	panic("implement me")
+func (r RedisCache) Exist(ctx context.Context, key string) bool {
+	_, err := r.client.Get(ctx, key).Result()
+	return err != nil
 }
 
-func (r RedisCache) Remove(key string) error {
-	//TODO implement me
-	panic("implement me")
+func (r RedisCache) Remove(ctx context.Context, key string) error {
+	return r.client.Del(ctx, key).Err()
 }
 
-func (r RedisCache) AddList(key string, isRight bool, value interface{}) error {
-	//TODO implement me
-	panic("implement me")
+func (r RedisCache) AddList(ctx context.Context, key string, isRight bool, value interface{}) error {
+	if isRight {
+		return r.client.RPush(ctx, key, value).Err()
+	} else {
+		return r.client.LPush(ctx, key, value).Err()
+	}
 }
 
-func (r RedisCache) ListPop(key string, isRight bool) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (r RedisCache) ListPop(ctx context.Context, key string, isRight bool) (string, error) {
+	if isRight {
+		return r.client.RPop(ctx, key).Result()
+	} else {
+		return r.client.LPop(ctx, key).Result()
+	}
 }
