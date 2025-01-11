3a40218b4dad7a393613d2f785baaa9c764f85b1
Hasan Erdal <hasaerda@hotmail.com>
2025-01-11 21:11:14 +0300

    maingo düzenlemesi

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/Filiphasan/golang-minify-url/configs"
 	"github.com/Filiphasan/golang-minify-url/internal/database"
 	"github.com/Filiphasan/golang-minify-url/internal/logger"
-	redisCache "github.com/Filiphasan/golang-minify-url/internal/redis"
+	"github.com/Filiphasan/golang-minify-url/internal/redis"
 	"github.com/Filiphasan/golang-minify-url/internal/setup"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
@@ -20,11 +20,11 @@ func main() {
 	appConfig := configs.GetAppConfig()
 
 	logger.UseLogger(appConfig)
-	mongo := database.UseMongo(appConfig, ctx)
-	redis := redisCache.UseRedis(appConfig, ctx)
+	mongoDb := database.UseMongo(appConfig, ctx)
+	redisCache := redis.UseRedis(appConfig, ctx)
 
 	router := gin.Default()
-	setupDefer := setup.NewApp(appConfig, logger.Logger, router, mongo, redis).SetupApp()
+	setupDefer := setup.NewApp(appConfig, logger.Logger, router, mongoDb, redisCache).SetupApp()
 	defer setupDefer(ctx)
 
 	logger.Logger.Info("Hello, Golang Minify URL!", zap.String("Owner", "Hasan Erdal"))
