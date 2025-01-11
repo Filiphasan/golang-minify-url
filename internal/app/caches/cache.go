b7259288112235115dcff2e0acd3c61501372a59
Hasan Erdal <hasaerda@hotmail.com>
2025-01-11 11:58:04 +0300

    redis düzenlemesi devam

--- internal/app/caches/cache.go
+++ internal/app/caches/cache.go
@@ -9,7 +9,7 @@ type Cache interface {
 	Ping(ctx context.Context) error
 	Get(ctx context.Context, key string) (string, error)
 	Set(ctx context.Context, key string, value interface{}, ttl time.Duration) error
-	Exist(ctx context.Context, key string) bool
+	Exist(ctx context.Context, key string) (bool, error)
 	Remove(ctx context.Context, key string) error
 	AddList(ctx context.Context, key string, isRight bool, value interface{}) error
 	ListPop(ctx context.Context, key string, isRight bool) (string, error)
